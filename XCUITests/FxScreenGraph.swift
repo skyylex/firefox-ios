2c6d95175ad7442c37b339c5bda3388df34fe9f9
isabelrios <isabelrios@gmail.com>
2017-03-17 13:07:48 +0100

    Bug 1347659 - Navigation XCUITests

--- XCUITests/FxScreenGraph.swift
+++ XCUITests/FxScreenGraph.swift
@@ -26,6 +26,7 @@ let URLBarOpen = "URLBarOpen"
 let NewPrivateTabScreen = "NewPrivateTabScreen"
 let PrivateTabTray = "PrivateTabTray"
 let PrivateBrowserTab = "PrivateBrowserTab"
+let FxSyncLogin = "FxSyncLogin"
 
 let allSettingsScreens = [
     SettingsScreen,
@@ -99,9 +100,16 @@ func createScreenGraph(_ app: XCUIApplication, url: String = "https://www.mozill
             }
 
             scene.tap(startBrowsingButton, to: NewTabScreen)
+            scene.tap(app.buttons["Sign in to Firefox"], to: FxSyncLogin)
         }
 
         i += 1
+    }
+
+    map.createScene(FxSyncLogin) { scene in
+        scene.backAction = {
+            app.buttons["Cancel"].tap()
+        }
     }
 
     map.createScene(NewTabScreen) { scene in
@@ -172,6 +180,7 @@ func createScreenGraph(_ app: XCUIApplication, url: String = "https://www.mozill
         scene.tap(table.cells["Logins"], to: LoginsSettings)
         scene.tap(table.cells["ClearPrivateData"], to: ClearPrivateDataSettings)
         scene.tap(table.cells["OpenWith.Setting"], to: OpenWithSettings)
+        scene.tap(table.cells["Show Tour"], to: Intro_Organize)
 
         scene.backAction = navigationControllerBackAction
     }
@@ -331,5 +340,11 @@ extension Navigator {
             self.goto(NewTabScreen)
             self.goto(TabTray)
         }
+    }
+
+    func changeBrowserView(view: String) {
+        self.goto(BrowserTabMenu)
+        let app = XCUIApplication()
+        app.collectionViews.cells[view].tap()
     }
 }
